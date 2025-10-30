2f102e399e6cbec70ffdd792d7c1132ffb76fa23
bogung kwon <95855658+kwon416@users.noreply.github.com>
2025-10-31 02:07:54 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 152ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 251031/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        print(n);
+    }
+
+    static void print(int n) {
+        if (n == 0) return;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+        print(n - 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}

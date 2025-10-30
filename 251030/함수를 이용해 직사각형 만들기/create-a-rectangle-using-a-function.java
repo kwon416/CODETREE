4123367f958fe455a312c44d7d9fe46b3137a6d7
bogung kwon <95855658+kwon416@users.noreply.github.com>
2025-10-30 17:32:37 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 179ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 251030/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rowNum = sc.nextInt();
+        int colNum = sc.nextInt();
+        // Please write your code here.
+        print(rowNum, colNum);
+    }
+
+    public static void print(int r, int c) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print("1");
+            }
+            System.out.println();
+        }
+    }
+}

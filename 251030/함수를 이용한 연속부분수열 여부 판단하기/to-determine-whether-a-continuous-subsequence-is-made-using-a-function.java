6b52dbcd9384006bfff930313f17cb9942a3ccfd
bogung kwon <95855658+kwon416@users.noreply.github.com>
2025-10-30 19:05:21 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251030/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int[] a = new int[n1];
+        int[] b = new int[n2];
+        for (int i = 0; i < n1; i++)
+            a[i] = sc.nextInt();
+        for (int i = 0; i < n2; i++)
+            b[i] = sc.nextInt();
+        // Please write your code here.
+        System.out.println(isConsecutive(a, b));
+
+
+    }
+
+    static String isConsecutive(int[] a, int[] b) {
+        int size = b.length;
+        for (int i = 0; i < a.length - size; i++) {
+            boolean find = false;
+            for (int j = 0; j < size; j++) {
+                
+                if (a[i + j] == b[j]) {
+                    if (j == size -1) find = true;
+                    continue;
+                }
+                else break;
+            }
+            if (find) {
+                return "Yes";
+            }
+        }
+        return "No";
+    }
+}

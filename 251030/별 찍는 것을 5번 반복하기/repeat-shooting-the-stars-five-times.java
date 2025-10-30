b61f9b253c6b580cc360c0d9a4b119b9d79ab1d8
bogung kwon <95855658+kwon416@users.noreply.github.com>
2025-10-30 17:28:57 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251030/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Please write your code here.
+        for (int i = 0; i < 5; i++) {
+            print();
+        }
+    }
+
+    public void print() {
+        System.out.println("**********");
+    }
+}

6f2a72bbeaf2f76c8245fa35b0aafcf92623c504
bogung kwon <95855658+kwon416@users.noreply.github.com>
2025-11-14 16:49:13 +0900

    Title: Date to Date, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251114/Date to Date/date-to-date.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        // Please write your code here.
+
+        int[] daysOfMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int days1 = 0;
+        for (int i = 0; i < m1 - 1; i++) {
+            days1 += daysOfMonth[i];
+        }
+        days1 += d1;
+
+        int days2 = 0;
+        for (int i = 0; i < m2 - 1; i++) {
+            days2 += daysOfMonth[i];
+        }
+        days2 += d2;
+
+        System.out.println(Math.abs(days1 - days2) + 1);
+    }
+}

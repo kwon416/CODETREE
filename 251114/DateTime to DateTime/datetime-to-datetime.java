b6de48b012ccc47ac9b1ebc538159ee0ca06decb
bogung kwon <95855658+kwon416@users.noreply.github.com>
2025-11-14 16:55:02 +0900

    Title: DateTime to DateTime, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251114/DateTime to DateTime/datetime-to-datetime.java
+++ 251114/DateTime to DateTime/datetime-to-datetime.java
@@ -8,6 +8,6 @@ public class Main {
         // Please write your code here.
         int min1 = 11 * 24 * 60 + 11 * 60 + 11;
         int min2 = A * 24 * 60 + B * 60 + C;
-        System.out.println(Math.abs(min1 - min2));
+        System.out.println(min1 > min2 ? Math.abs(min1 - min2) : -1);
     }
 }

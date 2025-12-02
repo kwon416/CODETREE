7d29f77a83b489f95662b849f05fddc44b3f7f0f
bogung kwon <95855658+kwon416@users.noreply.github.com>
2025-12-02 18:33:49 +0900

    Title: 사각형의 총 넓이 2, Time: 126ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 251202/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] x1 = new int[n];
+        int[] y1 = new int[n];
+        int[] x2 = new int[n];
+        int[] y2 = new int[n];
+        int[][] area = new int[201][201];
+        for (int i = 0; i < n; i++) {
+            x1[i] = sc.nextInt();
+            y1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+            y2[i] = sc.nextInt();
+
+            setArea(area, x1[i], x2[i], y1[i], y2[i]);
+        }
+        // Please write your code here.
+        int sum = 0;
+        for (int [] a : area) {
+            for (int i : a) {
+                if (i == 1) {
+                    sum++;
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+
+    public static void setArea(int[][] area, int x1, int x2, int y1, int y2) {
+
+        for (int i = x1; i < x2; i++) {
+            for (int j = y1; j < y2; j++) {
+                area[i][j] = 1;
+            }
+        }
+    }
+}

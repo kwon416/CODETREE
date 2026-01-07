f4a921350f1df14f4eea09992fc54a5f626ddbe9
bogung kwon <95855658+kwon416@users.noreply.github.com>
2026-01-08 02:25:54 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 260108/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
+++ 260108/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -1,70 +1,82 @@
 import java.util.Scanner;
-public class Main {
-    public static int K;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        K = sc.nextInt();
-        int P = sc.nextInt();
-        int T = sc.nextInt();
+import java.util.Arrays;
 
-        Developer[] developers = new Developer[100];
-
-        for (int i = 1; i <= N; i++) {
-            developers[i] = new Developer(i, false, 0);
-        }
-
-        developers[P].infect();
-
-        int[][] arr = new int[251][2];
-
-        for (int i = 0; i < T; i++) {
-            int t = sc.nextInt();
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-
-            arr[t] = new int[]{x, y};
-        }
-        // Please write your code here.
-
-        for (int i = 1; i < 251; i++) {
-            if (arr[i][0] != 0 && arr[i][1] != 0) {
-                developers[arr[i][0]].contag(developers[arr[i][1]]);
-            }
-        }
-
-        for (Developer d : developers) {
-            if (d != null) {
-                int ans = 0;
-                if (d.isInfect) ans = 1;
-                System.out.print(ans);
-            }
-        }
+// 악수의 정보를 나타내는 클래스 선언
+class Shake implements Comparable<Shake> {
+    int time;
+    int person1;
+    int person2;
+    
+    public Shake(int time, int person1, int person2) {
+        this.time = time;
+        this.person1 = person1;
+        this.person2 = person2;
     }
 
-    public static class Developer {
-        int index;
-        boolean isInfect;
-        int k;
+    @Override
+    public int compareTo(Shake shake) {
+        // 시간을 기준으로 오름차순으로 정렬합니다.
+        return time - shake.time;
+    }
+};
 
-        public Developer(int index, boolean isInfect, int k) {
-            this.index = index;
-            this.isInfect = isInfect;
-            this.k = k;
-        }
+public class Main {
+    public static final int MAX_T = 250;
+    public static final int MAX_N = 100;
+    
+    public static int n, k, p, t;
+    public static int[] shakeNum = new int[MAX_N + 1];
+    public static boolean[] infected = new boolean[MAX_N + 1];
 
-        public void infect() {
-            this.isInfect = true;
-            this.k = K;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력
+        n = sc.nextInt();
+        k = sc.nextInt();
+        p = sc.nextInt();
+        t = sc.nextInt();
+        infected[p] = true;
+        
+        Shake[] shakes = new Shake[MAX_T];
+        
+        for(int i = 0; i < t; i++) {
+            int time = sc.nextInt();
+            int person1 = sc.nextInt();
+            int person2 = sc.nextInt();
+            // Shake 객체를 생성해 넣어줍니다.
+            shakes[i] = new Shake(time, person1, person2);
         }
-
-        public void contag(Developer y) {
-            if (this.k > 0) {
-                this.k--;
-                if (!y.isInfect) {
-                    y.infect();
-                }
-            }
+        
+        // custom comparator를 활용한 정렬
+        Arrays.sort(shakes, 0, t);
+        
+        // 각 악수 횟수를 세서,
+        // K번 초과로 악수를 했을 시 전염시키지 않습니다.
+        for(int i = 0; i < t; i++) {
+            int target1 = shakes[i].person1;
+            int target2 = shakes[i].person2;
+            
+            // 감염되어 있을 경우 악수 횟수를 증가시킵니다.
+            if(infected[target1])
+                shakeNum[target1]++;
+            if(infected[target2])
+                shakeNum[target2]++;
+            
+            // target1이 감염되어 있고 아직 K번 이하로 악수했다면 target2를 전염시킵니다.
+            if(shakeNum[target1] <= k && infected[target1])
+                infected[target2] = true;
+            
+            // target2가 감염되어 있고 아직 K번 이하로 악수했다면 target1을 전염시킵니다.
+            if(shakeNum[target2] <= k && infected[target2])
+                infected[target1] = true;
         }
+        
+        for(int i = 1; i <= n; i++) {
+            if(infected[i])
+                System.out.print(1);
+            else
+                System.out.print(0);
+        }
+        
     }
 }

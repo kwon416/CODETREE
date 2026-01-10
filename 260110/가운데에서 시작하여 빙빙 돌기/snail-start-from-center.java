b4b07c33379fd943237b84981f96f592e04f7e3a
bogung kwon <95855658+kwon416@users.noreply.github.com>
2026-01-10 13:25:27 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 214ms, Memory: 12MB, Status: Passed - Codetree

--- 260110/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.java
+++ 260110/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int[] dx = new int[]{0, 1, 0, -1};
-    public static int[] dy = new int[]{1, 0, -1, 0};
+    public static int[] dx = new int[]{0, -1, 0, 1};
+    public static int[] dy = new int[]{-1, 0, 1, 0};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // Please write your code here.
-        int x = n / 2;
-        int y = n / 2;
+        int x = n- 1;
+        int y = n - 1;
         int dir = 0;
         int[][] grid = new int[n][n];
-        gird[x][y] = 1;
+        grid[x][y] = n * n;
 
-        for (int i = 2; i <= n * n; i++) {
+        for (int i = n * n - 1; i > 0; i--) {
             int nx = x + dx[dir];
             int ny = y + dy[dir];
 
@@ -23,12 +23,12 @@ public class Main {
             }
             x += dx[dir];
             y += dy[dir];
-            gird[x][y] = i;
+            grid[x][y] = i;
         }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(gird[i][j]);
+                System.out.print(grid[i][j] + " ");
             }
             System.out.println();
         }

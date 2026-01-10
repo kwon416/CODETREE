5dab7119e41ab873ebc15d9cf73e900606ecdc02
bogung kwon <95855658+kwon416@users.noreply.github.com>
2026-01-10 15:53:06 +0900

    Title: 체크판위에서 2, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

--- 260110/체크판위에서 2/on-the-checkboard-2.java
+++ 260110/체크판위에서 2/on-the-checkboard-2.java
@@ -16,11 +16,11 @@ public class Main {
         char c = grid[x][y];
         int cnt = 0;
 
-        for (int i = 1; i < R - 1; i++) {
-            for (int j = 1; j < C - 1; j++) {
+        for (int i = 1; i < R - 2; i++) {
+            for (int j = 1; j < C - 2; j++) {
                 for (int k = i + 1; k < R - 1; k++) {
                     for (int l = j + 1; l < C - 1; l++) {
-                        if (isDiff(c, grid[i][j]) && isDiff(grid[i][j], grid[k][l])) {
+                        if (isDiff(c, grid[i][j]) && isDiff(grid[i][j], grid[k][l]) && isDiff(grid[k][l], grid[R-1][C-1])) {
                             cnt++;
                         }
                     }

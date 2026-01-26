98cd166e236e62290965e33f9fecc14d520fcf46
bogung kwon <95855658+kwon416@users.noreply.github.com>
2026-01-26 20:14:41 +0900

    Title: G or H 3, Time: 136ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260126/G or H 3/G-or-H-3.java
+++ 260126/G or H 3/G-or-H-3.java
@@ -5,19 +5,19 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
         
-        int[] arr = new int[10001];
+        int[] arr = new int[10000];
 
         for (int i = 0; i < n; i++) {
-            int pos = sc.nextInt();
+            int pos = sc.nextInt() - 1;
             char c = sc.next().charAt(0);
-            arr[pos] = c == 'K' ? 1 : 2;
+            arr[pos] = c == 'G' ? 1 : 2;
         }
         // Please write your code here.
         int ans = 0;
         for (int i = 0; i <= n - k; i++) {
             int sum = 0;
-            for (int j = i; j < i + k; j++) {
-                sum += arr[j + 1];
+            for (int j = i; j <= i + k; j++) {
+                sum += arr[j];
             }
             ans = Math.max(ans, sum);
         }

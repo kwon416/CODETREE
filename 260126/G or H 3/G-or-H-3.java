5fd1c81e8555293e3bdcffa1eb0b7a1dc6d28894
bogung kwon <95855658+kwon416@users.noreply.github.com>
2026-01-26 20:17:33 +0900

    Title: G or H 3, Time: 134ms, Memory: 10MB, Status: Passed - Codetree

--- 260126/G or H 3/G-or-H-3.java
+++ 260126/G or H 3/G-or-H-3.java
@@ -5,16 +5,16 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
         
-        int[] arr = new int[10000];
+        int[] arr = new int[10000 + 1];
 
         for (int i = 0; i < n; i++) {
-            int pos = sc.nextInt() - 1;
+            int pos = sc.nextInt();
             char c = sc.next().charAt(0);
             arr[pos] = c == 'G' ? 1 : 2;
         }
         // Please write your code here.
         int ans = 0;
-        for (int i = 0; i <= n - k; i++) {
+        for (int i = 0; i <= 10000 - k; i++) {
             int sum = 0;
             for (int j = i; j <= i + k; j++) {
                 sum += arr[j];

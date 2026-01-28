72ccdbf027939a522c9a7bf5f7728ddc21815ecf
bogung kwon <95855658+kwon416@users.noreply.github.com>
2026-01-29 01:08:44 +0900

    Title: 개발팀의 능력, Time: 115ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260129/개발팀의 능력/the-capabilities-of-the-development-team.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Main {
+    public static int[] abil;
+    public static int sum;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        abil = new int[5];
+        sum = 0;
+        for (int i = 0; i < 5; i++) {
+            abil[i] = sc.nextInt();
+            sum += abil[i];
+        }
+        // Please write your code here.
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < 5; i++) {
+            for (int j = i + 1; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                    if (k == i || k == j) continue;
+                    for (int l = k + 1; l < 5; l++) {
+                        if (l == i || l == j) continue;
+                        int diff = getDiff(i, j, k, l);
+                        if (diff != 0) {
+                            min = Math.min(min, diff);
+                        }
+                    }
+                }
+            }
+        }
+        System.out.print(min);
+    }
+
+    public static int getDiff(int i, int j, int k, int l) {
+        int team1 = abil[i] + abil[j];
+        int team2 = abil[k] + abil[l];
+        int team3 = sum - team1 - team2;
+        if (team1 == team2 || team2 == team3 || team3 == team1) return 0;
+
+        int max = Math.max(team1, Math.max(team2, team3));
+        int min = Math.min(team1, Math.min(team2, team3));
+        return max - min;
+    }
+}

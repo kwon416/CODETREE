e53d89b96bfaf831e19cb3c7489075c550a98bb5
bogung kwon <95855658+kwon416@users.noreply.github.com>
2026-01-29 17:40:03 +0900

    Title: 겹치지 않는 선분 2, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260129/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] x1 = new int[n];
+        int[] x2 = new int[n];
+        for (int i = 0; i < n; i++) {
+            x1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            boolean overlap = false;
+
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+
+                if ((x1[i] <= x1[j] && x2[i] >= x2[j]) || (x1[i] >= x1[j] && x2[i] <= x2[j])) {
+                    overlap = true;
+                    break;
+                }
+            }
+            if (!overlap) {
+                ans++;
+            }
+        }
+        System.out.print(ans);
+    }
+}

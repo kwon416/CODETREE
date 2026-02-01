f121df956171e28735a95589cbb452e47a96d283
bogung kwon <95855658+kwon416@users.noreply.github.com>
2026-02-01 14:37:54 +0900

    Title: 독서실의 거리두기 5, Time: 113ms, Memory: 10MB, Status: Passed - Codetree

--- 260201/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
+++ 260201/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
@@ -1,27 +1,43 @@
 import java.util.Scanner;
+
 public class Main {
+    public static int n;
+    public static char[] seat;
+
+    public static int getMinDist() {
+        int minDist = n;
+        // 둘 다 1인 곳에 대해
+        // 모든 쌍을 조사하여, 그 중 가장 가까운 거리를 구합니다.
+        for(int i = 0; i < n; i++)
+            for(int j = i + 1; j < n; j++)
+                if(seat[i] == '1' && seat[j] == '1')
+                    minDist = Math.min(minDist, j - i);
+        
+        return minDist;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String seat = sc.next();
-        // Please write your code here.
+
+        // 입력
+        n = sc.nextInt();
+        seat = sc.next().toCharArray();
+
         int ans = 0;
-        for (int i = 0; i < seat.length(); i++) {
-            if (seat.charAt(i) == '1') continue;
-
-            String newSeat = seat.substring(0, i) + "1" + seat.substring(i + 1, n);
-
-            int idx = 0;
-            int dist = Integer.MAX_VALUE;
-            for (int j = 1; j < seat.length(); j++) {
-                if (newSeat.charAt(j) == '1') {
-                    dist = Math.min(dist, j - idx);
-                    idx = j;
-                }
+        // 들어갈 위치를 일일이 정해보며
+        // 그 상황에서 가장 가까운 사람간의 거리를 구해
+        // 가능한 경우 중 최댓값을 계산합니다.
+        for(int i = 0; i < n; i++) {
+            if(seat[i] == '0') {
+                // 비어있는 위치에 인원을 배치합니다.
+                seat[i] = '1';
+                // 가장 가까운 사람간의 거리를 구해 최댓값을 갱신해줍니다.
+                ans = Math.max(ans, getMinDist());
+                // 다시 채워졌던 값을 되돌려줍니다.
+                seat[i] = '0';
             }
-
-            ans = Math.max(ans, dist);
         }
-        System.out.print(ans);
+        
+        System.out.println(ans);
     }
 }

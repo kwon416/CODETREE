c0ac25b6f980575e5e8e442381d75798ea7c73eb
bogung kwon <95855658+kwon416@users.noreply.github.com>
2026-02-14 19:40:00 +0900

    Title: 이상한 폭탄 3, Time: 142ms, Memory: 14MB, Status: Passed - Codetree

--- 260214/이상한 폭탄 3/strange-bomb-3.java
+++ 260214/이상한 폭탄 3/strange-bomb-3.java
@@ -1,36 +1,58 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int BOMB_NUMS = 1000000;
+    public static final int MAX_A = 1000000;
+    public static final int MAX_N = 100;
+    
+    public static int n, k;
+    public static int[] num = new int[MAX_N];
+    public static int[] bomb = new int[MAX_A + 1];
+    public static boolean[] explode = new boolean[MAX_N];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int[] bombs = new int[n];
-        for (int i = 0; i < n; i++)
-            bombs[i] = sc.nextInt();
-        // Please write your code here.
-        int max = 0;
-        int index = 0;
-        for (int i = BOMB_NUMS; i > 0; i--) {
-            int[] dist = new int[n];
-            int cnt = 0;
-            for (int j = 1; j < n; j++) {
-                if (bombs[j] == i) {
-                    dist[cnt++] = j;    
+        // 입력
+        n = sc.nextInt();
+        k = sc.nextInt();
+        
+        for(int i = 0; i < n; i++)
+            num[i] = sc.nextInt();
+
+        int maxval = 1;
+        int maxidx = 0;
+
+        // 모든 쌍에 대해서 터질 수 있는 폭탄을 찾고
+        // 터진 폭탄의 개수를 저장합니다.
+        for(int i = 0; i < n; i++)
+            for(int j = i + 1; j < n; j++) {
+                // 거리가 k를 초과하는 경우 넘어갑니다.
+                if(j - i > k)
+                    break;
+
+                // 두 폭탄의 번호가 다를 경우 터지지 않습니다.
+                if(num[i] != num[j])
+                    continue;
+
+                // 두 폭탄의 번호가 같을 경우 폭탄은 터집니다.
+                // 해당 폭탄이 이미 터진 폭탄인지 확인하고,
+                // 아직 터지지 않은 폭탄이라면 터진 폭탄의 개수를 갱신합니다.
+                if(explode[i] == false) {
+                    bomb[num[i]]++;
+                    explode[i] = true;
+                }
+
+                if(explode[j] == false) {
+                    bomb[num[j]]++;
+                    explode[j] = true;
                 }
             }
-            int explode = 0;
-            for (int j = 1; j < cnt - 1; j++) {
-                if (dist[j] - dist[j - 1] <= k || dist[j + 1] - dist[j] <= k) {
-                    explode++;
-                }
+        
+        for(int i = 0; i <= MAX_A; i++)
+            if(maxval <= bomb[i]) {
+                maxval = bomb[i];
+                maxidx = i;
             }
-            if (max < explode) {
-                max = explode;
-                index = i;
-            }
-        }
-        System.out.print(index);
+        
+        System.out.print(maxidx);
     }
 }

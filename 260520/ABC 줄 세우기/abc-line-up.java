d117f02d05844fc8c2724260972dbc49b78436e2
bogung kwon <95855658+kwon416@users.noreply.github.com>
2026-05-20 15:19:40 +0900

    [Passed] ABC 줄 세우기 (132ms, 12MB)

---
+++ 260520/ABC 줄 세우기/abc-line-up.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char[] arr = new char[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next().charAt(0);
+        }
+        // Please write your code here.
+        int cnt = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    char temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    cnt++;
+                }
+            }
+        }
+        System.out.print(cnt);
+    }
+}

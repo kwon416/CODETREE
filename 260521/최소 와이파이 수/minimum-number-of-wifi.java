da1f12ef5c4e8118658368be057fbea9c43323e6
bogung kwon <95855658+kwon416@users.noreply.github.com>
2026-05-21 16:12:03 +0900

    [Passed] 최소 와이파이 수 (150ms, 10MB)

---
+++ 260521/최소 와이파이 수/minimum-number-of-wifi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        // Please write your code here.
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 1) {
+                i += m * 2;
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}

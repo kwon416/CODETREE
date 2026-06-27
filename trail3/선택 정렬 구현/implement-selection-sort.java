bd712fe828da9e7e46eb7ca419ee0b1dc0629374
bogung kwon <95855658+kwon416@users.noreply.github.com>
2026-06-27 19:45:51 +0900

    [Passed] 선택 정렬 구현 (148ms, 10MB)

---
+++ trail3/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] =temp;
+            }
+            
+        }
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+     }
+}
